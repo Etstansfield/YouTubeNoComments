de733e040e5a65c174e25a39624a073ebf393d02
Etstansfield <etstansfield@hotmail.com>
2021-03-03 19:58:13 +0000

    First Commit
    Browser Addon to remove Youtube Comments

---
+++ youtubenocomments.js
@@ -0,0 +1,15 @@
+
+// attaching a listener to page load didn't seem to work for some reason - so doing it the lazy way
+var execute = function () {
+ var commentsTag = document.getElementById('comments');
+  console.log(commentsTag);
+ if (commentsTag) {
+	 commentsTag.remove();
+  }
+
+ setTimeout(execute, 1000);
+};
+
+// Incase it removes it for some reason - it seems to do this on large vids
+setTimeout(execute, 1000);
+
